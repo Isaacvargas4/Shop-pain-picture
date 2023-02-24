fc4cfcda85792622c4543d4f60c5026b8de072d0
JUAN ISAAC VARGAS ESQUER <107451142+Isaacvargas4@users.noreply.github.com>
2023-02-24 12:31:22 -0700

    Add files via upload

---
+++ DutchTreat/DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,11 @@
+﻿
+
+let theForm = document.getElementById("theForm");
+console.log(theForm)
+theForm.hidden = true
+
+let button = document.getElementById("buyButton")
+
+button.addEventListener("click", () => theForm.hidden = false)
+
+
