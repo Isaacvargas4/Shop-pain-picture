99c610597ae4fd687576af60e65c81e49d0358ce
ivargas <ivargas@PCPSE0295>
2023-02-28 17:35:43 -0700

    Section 6 finished

--- DutchTreat/DutchTreat/Controllers/AppController.cs
+++ DutchTreat/DutchTreat/Controllers/AppController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,22 +9,42 @@ namespace DutchTreat.Controllers
 {
     public class AppController : Controller
     {
+
+
         public IActionResult Index()
         {
             //throw new InvalidProgramException("Bad things happen to good developers");
             return View();
         }
 
+        [HttpGet("contact")]
         public IActionResult Contact()
         {
-            ViewBag.Title = "Contact Us";
+
             //throw new InvalidProgramException("Bad things happen to good developers");
             return View();
         }
 
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //Send email
+                _mailService.SendMail
+
+
+            }
+
+
+            return View();
+
+        }
+
+        [HttpGet("about")]
         public IActionResult About()
         {
-            ViewBag.Title = "About us";
+
 
             return View();
         }
---
+++ DutchTreat/DutchTreat/ViewModels/ContactViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DutchTreat.ViewModels
+{
+    public class ContactViewModel
+    {
+        [Required]
+        [MinLength(5)]
+        public string Name { set; get; }
+        [Required]
+        [EmailAddress]
+        public string Email { set; get; }
+        [Required]
+        public string Subject { set; get; }
+        [Required]
+        [MaxLength(250, ErrorMessage = "Too long")]
+        public string Message { set; get; }
+
+    }
+}
