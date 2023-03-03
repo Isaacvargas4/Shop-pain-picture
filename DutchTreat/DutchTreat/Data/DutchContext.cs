f7e10f1adf752e3691a8e0b5ab24e55d02ebbb8d
ivargas <ivargas@PCPSE0295>
2023-03-03 12:05:12 -0700

    Started to built the database

---
+++ DutchTreat/DutchTreat/Data/DutchContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Unipluss.Sign.ExternalContract.Entities;
+using DutchTreat.Data.Entities;
+
+namespace DutchTreat.Data
+{
+    public class DutchContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnConfiguring( DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer();
+        }
+    }
+}
